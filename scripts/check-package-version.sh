9cf7c1388743a19f6a819f451360648663d28308
Milosz Galazka <milosz@sleeplessbeastie.eu>
2016-10-09 21:00:59 +0000

    Added check-package-version.sh shell script

---
+++ scripts/check-package-version.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+# perform
+
+# required package version
+required_package="apache2-utils"
+required_version="12.4.10-10+deb8u7"
+
+# local package version
+local_version=$(/usr/bin/dpkg-query -W -f="\${Version}\n" $required_package 2>/dev/null)
+
+# verify package version
+case $local_version in
+  "")
+     echo "Package \"$required_package\" is not installed"
+     ;;
+  $required_version)
+     echo "Package \"$required_package\" is installed, version constraint is met (\"$required_version\")"
+     ;;
+  *)
+     echo "Package \"$required_package\" is installed, version constraint is not met (\"$local_version\" != \"$required_version\")"
+     ;;
+esac
